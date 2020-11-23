7b407bd2b837f9cd6cb0f024113821c9f351e311
istvan_munkacsi <istvmunkacsi@gmail.com>
2020-11-23 23:28:01 +0100

    Setup navigation

---
+++ frontend/app/src/main/java/com/wsiz/projekt_zespolowy/ExampleFragment2.kt
@@ -0,0 +1,28 @@
+package com.wsiz.projekt_zespolowy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.example_fragment_layout.*
+
+class ExampleFragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.example_fragment_2_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            (activity as MainActivity).getNavController()
+                .navigate(R.id.action_exampleFragment2_to_exampleFragment3)
+        }
+    }
+}
--- frontend/app/src/main/java/com/wsiz/projekt_zespolowy/MainActivity.kt
+++ frontend/app/src/main/java/com/wsiz/projekt_zespolowy/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.wsiz.projekt_zespolowy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(getNavController())
+    }
+
+    fun getNavController(): NavController {
+        val navigationHost: NavHostFragment =
+                supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        return navigationHost.navController
     }
 }
